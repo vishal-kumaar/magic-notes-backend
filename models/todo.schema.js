7ecaad0bfd8777a82cbe8559bfdf16aaeb9da3e9
Vishal Kumar <vy10607@gmail.com>
2023-01-14 19:29:34 +0530

    Created todo schema

---
+++ models/todo.schema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        maxLength: [50, "Title must be at most 50 characters"],
+        trim: true,
+    },
+    tasks: [
+        {
+            task: {
+                type: String,
+                required: true,
+            },
+            checked: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+},
+{
+    timestamps: true,
+});
