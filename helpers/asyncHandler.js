7e2c23b4fa34ae36e2817ba97fab613a1dc981ab
Vishal Kumar <vy10607@gmail.com>
2023-01-14 21:23:02 +0530

    Fixed asyncHandler function

--- helpers/asyncHandler.js
+++ helpers/asyncHandler.js
@@ -1,20 +1,10 @@
-// export const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next);
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//         });
-//     }
-// };
-
-export function asyncHandler(fn){
-    return async function(){
-        try {
-            await fn(req, res, next);
-        } catch (error) {
-            
-        }
+export const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        });
     }
-}
+};
