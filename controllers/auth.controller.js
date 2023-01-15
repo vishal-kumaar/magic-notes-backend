7693c27e8ae2146cde145cb25c4107f686d80f48
Vishal Kumar <vy10607@gmail.com>
2023-01-15 13:08:05 +0530

    Auth controller - forgot password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.schema";
 import asyncHandler from "../helpers/asyncHandler";
 import CustomError from "../utils/CustomError"
 import cookieOptions from "../../../Mega_Project/utils/cookieOptions";
-import user from "../../../Full Auth System/model/user";
+import mailSender from "../utils/mailSender";
 
 /***************************************************
  * @SIGNUP
@@ -101,4 +101,54 @@ export const logOut = asyncHandler(async(req, res) => {
         success: true,
         message: "Logged out",
     });
+});
+
+/***************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:4000/api/auth/password/forgot
+ * @description User forgot password controller for forgot the password
+ * @description User submit email to generate a token
+ * @description User receives an url to forgot the password
+ * @parameters email
+ * @return Success message
+ ************************************************/
+
+export const forgotPassword = asyncHandler(async(req, res) => {
+    const {email} = req.body;
+    
+    if (!email){
+        throw new CustomError("Email is required",  400);
+    }
+
+    const user = await User.findOne({email});
+
+    if (!user){
+        throw new CustomError("User is not found", 400);
+    }
+
+    const resetToken = user.generateForgotPasswordToken();
+    user.save({validateBeforeSave: true});
+
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/auth/password/reset/${resetToken}`;
+    const text = `Your password reset url is\n\n${resetUrl}\n\nClick to reset the password and it is valid for 20 minutes`;
+
+    try {
+        mailSender({
+            email,
+            subject: `Password reset mail for ${req.get("host")}`,
+            text: text,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Mail send to ${email}`,
+        });
+    } catch (error) {
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordExpiry = undefined;
+
+        await user.save({validateBeforeSave: true});
+        
+        throw new CustomError(error.message || "Failed to send mail", 400);
+    }
 });
