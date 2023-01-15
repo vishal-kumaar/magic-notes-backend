2648234b82b60bc42295bc583e630b3f03efc233
Vishal Kumar <vy10607@gmail.com>
2023-01-15 12:49:01 +0530

    Create transporter config for nodemailer

--- config/config.js
+++ config/config.js
@@ -6,6 +6,11 @@ const config = {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USERNAME: process.env.SMTP_USERNAME,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_EMAIL: process.env.SMTP_EMAIL,
 }
 
 export default config;
