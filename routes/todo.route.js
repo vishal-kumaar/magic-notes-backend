d8c1d948e8ff3626de7d8de0ba73f79a25aff440
Vishal Kumar <vy10607@gmail.com>
2023-01-17 19:56:39 +0530

    Created todo route

---
+++ routes/todo.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    getAllTodos,
+    createTodo,
+    checkTask,
+    editTodoTitle,
+    editTask,
+    deleteTodo
+} from "../controllers/todo.controller";
+
+const router = express.Router();
+
+router.get("/api/todo", getAllTodos);
+router.post("/api/todo/createTodo", createTodo);
+router.put("/api/todo/checkTodo/:todoId", checkTask);
+router.put("/api/todo/editTitle/:todoId", editTodoTitle);
+router.put("/api/todo/editTask/:todoId", editTask);
+router.delete("/api/todo/deleteTodo/:todoId", deleteTodo);
+
+export default router;
