d7131adc3b5003f473812eeb250d104e99f595b9
Vishal Kumar <vy10607@gmail.com>
2023-01-19 16:52:04 +0530

    Added isLoggedIn middleware in todo and user route

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -7,14 +7,15 @@ import {
     editTask,
     deleteTodo
 } from "../controllers/todo.controller.js";
+import {isLoggedIn} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/api/todo", getAllTodos);
-router.post("/api/todo/createTodo", createTodo);
-router.put("/api/todo/checkTodo/:todoId", checkTask);
-router.put("/api/todo/editTitle/:todoId", editTodoTitle);
-router.put("/api/todo/editTask/:todoId", editTask);
-router.delete("/api/todo/deleteTodo/:todoId", deleteTodo);
+router.get("/api/todo", isLoggedIn, getAllTodos);
+router.post("/api/todo/createTodo",isLoggedIn, createTodo);
+router.put("/api/todo/checkTodo/:todoId",isLoggedIn, checkTask);
+router.put("/api/todo/editTitle/:todoId",isLoggedIn, editTodoTitle);
+router.put("/api/todo/editTask/:todoId",isLoggedIn, editTask);
+router.delete("/api/todo/deleteTodo/:todoId",isLoggedIn, deleteTodo);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,16 +8,16 @@ import {
     forgotPassword,
     getProfile
 } from "../controllers/auth.controller.js";
+import {isLoggedIn} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/api/auth/signup", signUp);
-router.get("/api/auth/logIn", logIn);
-router.get("/api/auth/logOut", logOut);
+router.get("/api/auth/login", logIn);
+router.get("/api/auth/logout",isLoggedIn, logOut);
 router.put("/api/auth/password/forgot", forgotPassword);
 router.put("/api/auth/password/reset/:resetToken", resetPassword);
-router.put("/api/auth/password/update/:id", updatePassword);
-router.put("/api/auth/password/update/:id", updatePassword);
-router.get("/api/auth/profile", getProfile);
+router.put("/api/auth/password/update/:id",isLoggedIn, updatePassword);
+router.get("/api/auth/profile",isLoggedIn, getProfile);
 
 export default router;
