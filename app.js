7795a2a8ac8f0eb99043f96fd0fdc073d4db210c
Vishal Kumar <vy10607@gmail.com>
2023-01-30 20:43:51 +0530

    Added frontend in app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import userRoute from "./routes/user.route.js";
 import todoRoute from "./routes/todo.route.js";
 import cookieParsar from "cookie-parser";
 import cors from "cors";
-import morgan from "morgan"
+import morgan from "morgan";
+import path from "path";
+import {fileURLToPath} from 'url';
+import config from "./config/config.js";
 
 const app = express();
 
@@ -13,7 +16,18 @@ app.use(cors());
 app.use(cookieParsar());
 app.use(morgan("tiny"));
 
-app.use("/", userRoute);
-app.use("/", todoRoute);
+if (config.NODE_ENV){
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    app.use(express.static(path.join(__dirname, "./frontend")));
+    app.use("/", userRoute);
+    app.use("/", todoRoute);
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "./frontend"));
+    })
+}else{
+    app.use("/", userRoute);
+    app.use("/", todoRoute);
+}
 
 export default app;
