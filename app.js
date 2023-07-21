d9f9fc8cb5cbea7733296447191e8c94ca8d1ec9
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 22:25:59 +0530

    Remove frontend from app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ import noteRoute from "./routes/note.route.js";
 import cookieParsar from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
-import {fileURLToPath} from 'url';
-import config from "./config/config.js";
 
 const app = express();
 
@@ -18,14 +15,5 @@ app.use(morgan("tiny"));
 
 app.use("/api/auth", userRoute);
 app.use("/api/note", noteRoute);
-
-if (config.NODE_ENV){
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    app.use(express.static(path.join(__dirname, "./frontend")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./frontend/index.html"));
-    })
-}
 
 export default app;
