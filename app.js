65efecb2feba0ce2eef932fe06a2f5dadad57f09
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 22:38:32 +0530

    Add cors config in app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import noteRoute from "./routes/note.route.js";
 import cookieParsar from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
+import corsConfig from "./config/cors.config.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(cookieParsar());
 app.use(morgan("tiny"));
 
--- config/config.js
+++ config/config.js
@@ -11,6 +11,7 @@ const config = {
     SMTP_PASSWORD: process.env.SMTP_PASSWORD,
     SMTP_EMAIL: process.env.SMTP_EMAIL,
     NODE_ENV: process.env.NODE_ENV,
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS
 }
 
 export default config;
