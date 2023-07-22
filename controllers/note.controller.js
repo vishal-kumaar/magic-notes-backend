0b7f34efe0ef801d46f943723378edbca5d34eae
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-22 10:40:28 +0530

    Update getAllNotes controller

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -1,7 +1,6 @@
 import Note from "../models/note.schema.js";
 import asyncHander from "../helpers/asyncHandler.js";
 import CustomError from "../utils/CustomError.js";
-import sortByDate from "../utils/sortByTime.js";
 
 /***************************************************
  * @CREATE_NOTE
@@ -12,26 +11,26 @@ import sortByDate from "../utils/sortByTime.js";
  * @return Note Object
  ************************************************/
 
-export const createNote = asyncHander(async(req, res) => {
-    const {title, body} = req.body;
-    if (!title){
-        throw new CustomError("Title is required", 400);
-    }
+export const createNote = asyncHander(async (req, res) => {
+  const { title, body } = req.body;
+  if (!title) {
+    throw new CustomError("Title is required", 400);
+  }
 
-    const note = await Note.create({
-        title,
-        body: "",
-        user: req.user._id,
-    });
+  const note = await Note.create({
+    title,
+    body: "",
+    user: req.user._id,
+  });
 
-    if (!note){
-       throw new CustomError("Server error! Try again", 500); 
-    }
+  if (!note) {
+    throw new CustomError("Server error! Try again", 500);
+  }
 
-    res.status(200).json({
-        success: true,
-        note
-    });
+  res.status(200).json({
+    success: true,
+    note,
+  });
 });
 
 /***************************************************
@@ -43,31 +42,31 @@ export const createNote = asyncHander(async(req, res) => {
  * @return Note Object
  ************************************************/
 
-export const editNote = asyncHander(async(req, res) => {
-    const {noteId} = req.params;
-    if (!noteId){
-        throw new CustomError("Note Id is required", 400);
-    }
+export const editNote = asyncHander(async (req, res) => {
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new CustomError("Note Id is required", 400);
+  }
 
-    const {title, body} = req.body;
-    if (!title) {
-        throw new CustomError("Title is required", 400);
-    }
+  const { title, body } = req.body;
+  if (!title) {
+    throw new CustomError("Title is required", 400);
+  }
 
-    const note = await Note.findById(noteId);
-    if (!note){
-        throw new CustomError("Note not found", 404);
-    }
+  const note = await Note.findById(noteId);
+  if (!note) {
+    throw new CustomError("Note not found", 404);
+  }
 
-    note.title = title;
-    note.body = body;
+  note.title = title;
+  note.body = body;
 
-    await note.save();
+  await note.save();
 
-    res.status(200).json({
-        success: true,
-        note
-    });
+  res.status(200).json({
+    success: true,
+    note,
+  });
 });
 
 /***************************************************
@@ -79,30 +78,29 @@ export const editNote = asyncHander(async(req, res) => {
  * @return Note Object
  ************************************************/
 
-export const checkNote = asyncHander(async(req, res) => {
-    const {noteId} = req.params;
-    if (!noteId){
-        throw new CustomError("Note Id is required", 400);
-    }
+export const checkNote = asyncHander(async (req, res) => {
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new CustomError("Note Id is required", 400);
+  }
 
-    const note = await Note.findById(noteId);
-    if (!note){
-        throw new CustomError("Note is not found", 404);
-    }
+  const note = await Note.findById(noteId);
+  if (!note) {
+    throw new CustomError("Note is not found", 404);
+  }
 
-    if (note.checked){
-        note.checked = false;
-    }
-    else{
-        note.checked = true;
-    }
+  if (note.checked) {
+    note.checked = false;
+  } else {
+    note.checked = true;
+  }
 
-    await note.save();
+  await note.save();
 
-    res.status(200).json({
-        succes: true,
-        note
-    })
+  res.status(200).json({
+    succes: true,
+    note,
+  });
 });
 
 /***************************************************
@@ -114,21 +112,21 @@ export const checkNote = asyncHander(async(req, res) => {
  * @return Note object
  ************************************************/
 
-export const getNote = asyncHander(async(req, res) => {
-    const {noteId} = req.params;
-    if (!noteId){
-        throw new CustomError("Note id is required", 400);
-    }
+export const getNote = asyncHander(async (req, res) => {
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new CustomError("Note id is required", 400);
+  }
 
-    const note = await Note.findById(noteId);
-    if (!note){
-        throw new CustomError("Note is not found", 404);
-    }
+  const note = await Note.findById(noteId);
+  if (!note) {
+    throw new CustomError("Note is not found", 404);
+  }
 
-    res.status(200).json({
-        success: true,
-        note
-    })
+  res.status(200).json({
+    success: true,
+    note,
+  });
 });
 
 /***************************************************
@@ -138,18 +136,18 @@ export const getNote = asyncHander(async(req, res) => {
  * @description User get all the notes controller
  * @parameters user id
  * @return Notes Object
-************************************************/
+ ************************************************/
 
-export const getAllNotes = asyncHander(async(req, res) => {
-    const notes = await Note.find({user: req.user._id});
-    if (notes.length === 0){
-        throw new CustomError("Notes not found", 404);
-    }
+export const getAllNotes = asyncHander(async (req, res) => {
+  const notes = await Note.find({ user: req.user._id }).sort({ createdAt: -1 });
+  if (notes.length === 0) {
+    throw new CustomError("Notes not found", 404);
+  }
 
-    res.status(200).json({
-        success: true,
-        notes: notes.sort(sortByDate)
-    });
+  res.status(200).json({
+    success: true,
+    notes: notes,
+  });
 });
 
 /***************************************************
@@ -161,35 +159,28 @@ export const getAllNotes = asyncHander(async(req, res) => {
  * @return Note object
  ************************************************/
 
-export const searchNotes = asyncHander(async(req, res) => {
-    const {input} = req.query;
+export const searchNotes = asyncHander(async (req, res) => {
+  const { input } = req.query;
 
-    const notes = await Note.find({ 
-        $or:[
-            {
-                $and:[
-                    {'title':  new RegExp(input, "i") },
-                    {user: req.user._id}
-                ]
-            },
-            {
-                $and:[
-                    {'body' : new RegExp(input, "i")},
-                    {user: req.user._id}
-                ]
-                
-            }
-        ]
-    });
+  const notes = await Note.find({
+    $or: [
+      {
+        $and: [{ title: new RegExp(input, "i") }, { user: req.user._id }],
+      },
+      {
+        $and: [{ body: new RegExp(input, "i") }, { user: req.user._id }],
+      },
+    ],
+  });
 
-    if (notes.length === 0){
-        throw new CustomError("Notes is not found", 404)
-    }
+  if (notes.length === 0) {
+    throw new CustomError("Notes is not found", 404);
+  }
 
-    res.status(200).json({
-        success: true,
-        notes: notes.reverse()
-    })
+  res.status(200).json({
+    success: true,
+    notes: notes.reverse(),
+  });
 });
 
 /***************************************************
@@ -201,20 +192,20 @@ export const searchNotes = asyncHander(async(req, res) => {
  * @return Success message and note object
  ************************************************/
 
-export const deleteNote = asyncHander(async(req, res) => {
-    const {noteId} = req.params;
-    if (!noteId){
-        throw new CustomError("Note id is required", 400);
-    }
+export const deleteNote = asyncHander(async (req, res) => {
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new CustomError("Note id is required", 400);
+  }
 
-    const note = await Note.findByIdAndDelete(noteId);
-    if (!note){
-        throw new CustomError("Note is not found", 404);
-    }
+  const note = await Note.findByIdAndDelete(noteId);
+  if (!note) {
+    throw new CustomError("Note is not found", 404);
+  }
 
-    res.status(200).json({
-        succes: true,
-        messase: "Note deleted successfully",
-        note
-    })
+  res.status(200).json({
+    succes: true,
+    messase: "Note deleted successfully",
+    note,
+  });
 });
